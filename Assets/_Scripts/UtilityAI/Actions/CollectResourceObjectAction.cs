2fb4388d6cc8bb9c98135625033dd7b129dd702a
dendefo <daniek332@gmail.com>
2025-08-05 01:29:24 +0300

    Drone is droning

---
+++ Assets/_Scripts/UtilityAI/Actions/CollectResourceObjectAction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UtilityAI/Actions/CollectResourceObjectAction")]
+public class CollectResourceObjectAction : AIAction
+{
+    public static Dictionary<UtilityAIAgent, ResourceHandler> AgentResourcePairs = new();
+    public override void Initialize(Context context)
+    {
+        context.sensor.AddTag(targetTag);
+        base.Initialize(context);
+    }
+    public override float CalculateUtility(Context context)
+    {
+        if (AgentResourcePairs.TryGetValue(context.brain, out var res) && res)
+        {
+            return 0;
+        }
+        return base.CalculateUtility(context);
+    }
+    public override void Execute(Context context)
+    {
+        var target = context.sensor.GetClosestTarget(targetTag);
+        if (!target) return;
+        var res = target.GetComponent<ResourceHandler>();
+        if (!res)
+        {
+            PerformActionOverTime(context, 2, null);
+            return;
+        }
+        AgentResourcePairs[context.brain] = res;
+        context.target = target;
+        target.SetParent(context.brain.transform);
+    }
+}
---
+++ Assets/_Scripts/UtilityAI/Actions/DeliverResource.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UtilityAI/Actions/DeliverResourceAction")]
+public class DeliverResource : MoveToTargetAIAction
+{
+    public override float CalculateUtility(Context context)
+    {
+        //if 
+        return base.CalculateUtility(context);
+    }
+    public override void Execute(Context context)
+    {
+        base.Execute(context);
+    }
+}
