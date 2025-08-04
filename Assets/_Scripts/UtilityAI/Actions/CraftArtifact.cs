abbdcc27434630295a20bb94e682b815ec393f80
yoavk-sudo <kenkendler123@gmail.com>
2025-08-05 02:28:49 +0300

    Add artifact crafting system and related assets
    Introduced new prefabs for Artifact, Shield, Staff, and Tree, along with their associated textures and meta files. Added scriptable objects for Shield and Staff resources, and a recipe for crafting the UltimateArtifact. Updated Villager prefab to include new crafting actions and modified GameScene to support these changes. Also added a new UtilityAI action for crafting artifacts and related consideration assets.

---
+++ Assets/_Scripts/UtilityAI/Actions/CraftArtifact.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UtilityAI/Actions/Craft")]
+public class CraftArtifact : AIAction
+{
+    [SerializeField] CraftingRecipeSO recipe;
+    [SerializeField] GameObject artifactPrefab;
+    public override void Execute(Context context)
+    {
+        foreach (var item in recipe.ingredients)
+        {
+            ResourceManager.Instance.UseResourceAmount(item.resource.name, item.amount);
+        }
+        Instantiate(artifactPrefab, ResourceManager.Instance.transform.position + Vector3.up * 0.5f, Quaternion.identity);
+        ResourceManager.Instance.GainResourceAmount(recipe.recipeName, 1);
+    }
+}
