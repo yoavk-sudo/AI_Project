b4bb619c32c8316dd3790fe2c5a15da0d0b36f7f
dendefo <daniek332@gmail.com>
2025-08-05 02:42:49 +0300

    Drone can pick up all resources (but not at the same time)

--- Assets/_Scripts/ResourceHandlers/RenewableResourceHandler.cs
+++ Assets/_Scripts/ResourceHandlers/RenewableResourceHandler.cs
@@ -27,7 +27,7 @@ public class RenewableResourceHandler : MonoBehaviour
         ObjectToTurnOff.gameObject.SetActive(false);
         readyToCollect = false;
         yield return new WaitForSeconds(TimeToTurnOff);
-        yield return new WaitUntil(() => Vector3.Distance(spawnedResource.transform.position, transform.position) > 1f);
+        yield return new WaitUntil(() => Vector3.Distance(spawnedResource.transform.position, spawnPosition.position) > 1f);
         collider.excludeLayers = 0;
         ObjectToTurnOff.gameObject.SetActive(true);
         readyToCollect = true;
