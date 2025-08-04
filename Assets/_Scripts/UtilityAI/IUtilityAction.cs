7b05de43708b9af15847b9b6a9ddcc1c2359cadb
danieldaniel128 <danielkaarur@gmail.com>
2025-08-04 18:41:20 +0300

    removed unused utility actions

--- Assets/_Scripts/UtilityAI/IUtilityAction.cs
+++
@@ -1,5 +0,0 @@
-public interface IUtilityAction
-{
-    float GetScore();
-    void Execute();
-}
--- Assets/_Scripts/UtilityAI/abstract_UtilityActionBase.cs
+++
@@ -1,5 +0,0 @@
-public abstract class UtilityActionBase : IUtilityAction
-{
-    public abstract float GetScore();
-    public abstract void Execute();
-}
